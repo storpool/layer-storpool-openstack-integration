4b46bf7827cbe5a6508871170ce4a385df30aafa
Peter Pentchev <pp@storpool.com>
2020-11-22 18:22:24 +0200

    Drop most of the StorPool config handling.

--- lib/spcharms/kvdata.py
+++ lib/spcharms/kvdata.py
@@ -3,16 +3,10 @@
 Constants for use with unitdata.kv().
 """
 
-KEY_OURID = "storpool-config.our-id"
-
 KEY_MACHINE_ID = "storpool-helper.machine-id"
 KEY_PARENT_NODE_ID = "storpool-helper.parent-node-id"
-KEY_SET_STATES = "storpool-helper.set-states"
-KEY_META_CONFIG = "storpool-helper.meta-config"
 KEY_META_GENERATION = "storpool-helper.meta-generation"
 
 KEY_LXD_NAME = "storpool-openstack-integration.lxd-name"
-
-KEY_PRESENCE = "storpool-service.state"
 
 KEY_SPSTATUS = "storpool-utils.persistent-status"
--- lib/spcharms/run/storpool_config.py
+++ lib/spcharms/run/storpool_config.py
@@ -3,8 +3,9 @@ A Juju layer for installing and configuring the base StorPool packages.
 """
 from __future__ import print_function
 
-from spcharms import config as spconfig
-from spcharms import status as spstatus
+import subprocess
+
+from spcharms import error as sperror
 from spcharms import utils as sputils
 
 from spcharms.run import storpool_repo_add as run_repo
@@ -24,35 +25,31 @@ def config_changed():
     rdebug("config-changed happened")
 
 
-def read_config():
-    """
-    Read the StorPool configuration file.
-    """
-    rdebug("about to read the /etc/storpool.conf file")
-    spstatus.npset("maintenance", "parsing the /etc/storpool.conf file")
-
-    # Remove any states that say we have accomplished anything...
-    spconfig.m()
-    spconfig.unset_our_id()
-    spconfig.drop_cache()
-
-    cfg = spconfig.get_dict()
-    oid = cfg["SP_OURID"]
-    spconfig.set_our_id(oid)
-    rdebug(
-        "got {len} keys in the StorPool config, our id is {oid}".format(
-            len=len(cfg), oid=oid
-        )
-    )
-
-    spstatus.npset("maintenance", "")
-
-
 def run():
     rdebug("Run, repo, run!")
     run_repo.run()
     rdebug("Returning to the storpool-config setup")
-    read_config()
+
+    try:
+        data = (
+            subprocess.check_output(
+                ["storpool_confshow", "-ne", "SP_OURID"], shell=False
+            )
+            .decode("UTF-8")
+            .splitlines()
+        )
+    except (IOError, subprocess.CalledProcessError):
+        raise sperror.StorPoolMissingComponentsError(["storpool_confshow"])
+
+    if len(data) != 1:
+        raise sperror.StorPoolMissingComponentsError(["SP_OURID"])
+
+    try:
+        our_id = int(data[0])
+    except ValueError:
+        raise sperror.StorPoolMissingComponentsError(["valid SP_OURID"])
+
+    rdebug("Got our StorPool ID {our_id}".format(our_id=our_id))
 
 
 def stop():
